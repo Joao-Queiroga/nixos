a890c4f746da02b9120c7a4e9bbbe69157f569f2
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-08-30 12:29:41 -0300

    tuxnote: add btrfs mount arguments

--- tuxnote/hardware-configuration.nix
+++ tuxnote/hardware-configuration.nix
@@ -4,54 +4,77 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
+    fsType = "btrfs";
+    options =
+      [ "subvol=@" "compress=zstd:1" "lazytime" "discard=async" "autodefrag" ];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
+    fsType = "btrfs";
+    options = [
+      "subvol=@home"
+      "compress=zstd:1"
+      "lazytime"
+      "discard=async"
+      "autodefrag"
+    ];
+  };
 
-  fileSystems."/var" =
-    { device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
-      fsType = "btrfs";
-      options = [ "subvol=@var" ];
-    };
+  fileSystems."/var" = {
+    device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
+    fsType = "btrfs";
+    options = [
+      "subvol=@var"
+      "compress=zstd:1"
+      "lazytime"
+      "discard=async"
+      "autodefrag"
+    ];
+  };
 
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
-      fsType = "btrfs";
-      options = [ "subvol=@log" ];
-    };
+  fileSystems."/var/log" = {
+    device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
+    fsType = "btrfs";
+    options = [
+      "subvol=@log"
+      "compress=zstd:1"
+      "lazytime"
+      "discard=async"
+      "autodefrag"
+    ];
+  };
 
-  fileSystems."/root" =
-    { device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
-      fsType = "btrfs";
-      options = [ "subvol=@root" ];
-    };
+  fileSystems."/root" = {
+    device = "/dev/disk/by-uuid/c1922fc6-523f-4fd0-bebf-465634a6e147";
+    fsType = "btrfs";
+    options = [
+      "subvol=@root"
+      "compress=zstd:1"
+      "lazytime"
+      "discard=async"
+      "autodefrag"
+    ];
+  };
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/64B8-EF51";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/64B8-EF51";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/29525ea8-917c-4e5c-a1b6-d2fa56a9b656"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/29525ea8-917c-4e5c-a1b6-d2fa56a9b656"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -62,5 +85,6 @@
   # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
