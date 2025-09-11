1f8c6bb5ff8070d9d5866a9ece00c0a1e4bc8dfe
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-11 19:57:47 -0300

    tux: add gaming packages

--- tux/configuration.nix
+++ tux/configuration.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
-  networking.hostName = "tux";
+  environment.systemPackages = with pkgs; [ lsfg-vk lsfg-vk-ui prismlauncher ];
 
   programs.steam = {
     enable = true;
