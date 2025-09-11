6ff2680264fd7814f76dd9b28ba3f0bfc0d11a53
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-11 19:58:15 -0300

    make DM use the home-manager version of Hyprland when available

--- common-configuration.nix
+++ common-configuration.nix
@@ -6,6 +6,12 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.use-xdg-base-directories = true;
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 
   boot = {
     plymouth.enable = true;
@@ -119,9 +125,21 @@
       enableCompletion = true;
     };
     nix-ld.enable = true;
-    hyprland = {
+    hyprland = { enable = true; };
+    uwsm = {
       enable = true;
-      withUWSM = true;
+      waylandCompositors = {
+        hyprland = {
+          prettyName = "Hyprland";
+          comment = "Hyprland compositor managed by UWSM";
+          binPath = "${
+              pkgs.writeShellScriptBin "Hyprland" ''
+                #!/bin/sh
+                exec Hyprland $@
+              ''
+            }/bin/Hyprland";
+        };
+      };
     };
     gamemode.enable = true;
     thunar.enable = true;
