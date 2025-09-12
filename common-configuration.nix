66cea6f5353110420c9fd70b1e67cb95ba5ebd93
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-11 22:14:01 -0300

    remove unecessary lines

--- common-configuration.nix
+++ common-configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, inputs, ... }: {
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -45,7 +41,6 @@
     ];
   };
 
-  # Use latest kernel.
   boot = {
     kernelPackages = pkgs.linuxPackages_cachyos-lto;
     extraModulePackages =
@@ -56,39 +51,23 @@
 
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Brazil/East";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "pt_BR.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "br-abnt2";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
   services.udisks2.enable = true;
   services.gvfs.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb.layout = "br";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  services.pipewire = {
-    enable = true;
-    #pulse.enable = true;
-  };
+  services.pipewire = { enable = true; };
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
@@ -99,9 +78,6 @@
     extraPackages = with pkgs; [ mesa vulkan-tools ];
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   programs.bash = {
     enable = true;
     interactiveShellInit = ''
@@ -109,7 +85,6 @@
     '';
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.defaultUserShell = pkgs.bash;
   users.users.joaoqueiroga = {
     isNormalUser = true;
@@ -145,8 +120,6 @@
     neovim.enable = true;
   };
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     vim
     wget
@@ -203,49 +176,12 @@
     targets.grub.useWallpaper = false;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
   networking.firewall = {
     checkReversePath = false;
     allowedTCPPorts = [ 443 ];
     allowedUDPPorts = [ 1194 ];
   };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
   system.autoUpgrade = {
     enable = true;
     flake = inputs.self.outPath;
@@ -253,4 +189,6 @@
     dates = "2:00";
     randomizedDelaySec = "45min";
   };
+
+  system.stateVersion = "25.05";
 }
