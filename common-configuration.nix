9f991d00beef9d116984fb8e7055e336b14c1345
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-11-16 13:39:59 -0300

    add wheel group to trusted users and add niri binary cache

--- common-configuration.nix
+++ common-configuration.nix
@@ -15,9 +15,10 @@
   ];
   nix.settings.use-xdg-base-directories = true;
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    trusted-users = ["root" "@wheel"];
+    substituters = ["https://hyprland.cachix.org" "https://niri.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org" "https://niri.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="];
   };
 
   boot = {
