b48ae0effaaa48f53f8cd5d2c2e0851123fc6b5e
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2026-05-31 21:28:48 -0300

    tux: habilita ddccontrol

--- modules/hosts/tux/configuration.nix
+++ modules/hosts/tux/configuration.nix
@@ -28,6 +28,8 @@
       QSG_RHI_BACKEND = "vulkan";
     };
 
+    services.ddccontrol.enable = true;
+
     boot = {
       extraModulePackages = with config.boot.kernelPackages; [
         ddcci-driver
@@ -35,15 +37,31 @@
       kernelModules = [
         "ddcci"
       ];
+      kernelParams = ["amdgpu.gttsize=4096" "transparent_hugepage=madvise"];
       kernel.sysctl = {
         "vm.swappiness" = 180;
         "vm.watermark_boost_factor" = 0;
         "vm.watermark_scale_factor" = 125;
         "vm.page-cluster" = 0;
+        "vm.min_free_kbytes" = 524288;
+        "vm.dirty_ratio" = 10;
+        "vm.dirty_background_ratio" = 5;
+        "vm.compaction_proactiveness" = 20;
+        "vm.lru_gen.enabled" = 7;
+        "vm.lru_gen.min_ttl_ms" = 1000;
       };
     };
-
-    zramSwap.memoryPercent = 90;
+    zramSwap.memoryPercent = 75;
+    services.earlyoom = {
+      enable = true;
+      freeMemThreshold = 5;
+      freeSwapThreshold = 5;
+      extraArgs = [
+        "-m 8,4"
+        "-s 8,4"
+      ];
+      enableNotifications = true;
+    };
 
     services.sunshine = {
       enable = true;
